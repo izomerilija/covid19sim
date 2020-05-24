cc04450ad672ec13790bfea0f36329c29830b79e
miks4 <62470347+miks4@users.noreply.github.com>
2020-05-24 20:01:49 +0200

    dodato dugme start sa 100%funkcionalnosti
    sredjen kod i uskladjeni parametri sa simulacijom

--- GUI/mainDef.py
+++ GUI/mainDef.py
@@ -6,119 +6,148 @@ from ScreenClass import *
 from ButtonClass import *
 #from simulation1 import *
 vec = pg.math.Vector2
+import time
+import multiprocessing
+import threading
 
 pg.init()
 
 
 
-def main():
+menuScreen = Screen("menu screen",600,400)
+tempScreen = Screen("temp screen",400,300)
+simScreen = Screen("Simulator",500,600,(0,0,0))
+mainClock = pg.time.Clock()
 
-    menuScreen = Screen("menu screen",600,400)
-    tempScreen = Screen("temp screen",400,300)
-    simScreen = Screen("Simulator",750,790)
-    mainClock = pg.time.Clock()
+myfont = pg.font.SysFont("Arial", 20)
+label = myfont.render(" Enter a number of people =>",1, (255,255,0))
+labelReady = Button(220,210,70,50,(255,0,0),"READY!")
+labelReturn = Button(150,170,70,50,(255,255,0),"RETURN!")
+labelCity = myfont.render(" Chose a real city =>",1, (255,255,0))
+dropButton = Button(199,156,80,15,(40,40,40),"                               ")
+myfont2 = pg.font.SysFont("Arial", 17)
+labelSee = myfont2.render(" click to see",1, (0,0,0))
 
-    myfont = pg.font.SysFont("Arial", 20)
-    label = myfont.render(" Enter a number of people =>",1, (255,255,0))
-    labelReady = Button(220,210,70,50,(255,0,0),"READY!")
-    labelReturn = Button(150,170,70,50,(255,255,0),"RETURN!")
-    labelCity = myfont.render(" Chose a real city =>",1, (255,255,0))
-    dropButton = Button(199,156,80,15,(40,40,40),"                               ")
-    myfont2 = pg.font.SysFont("Arial", 17)
-    labelSee = myfont2.render(" click to see",1, (0,0,0))
+startButton = Button(0,500,150,50,(0,0,255),"START",(0,0,0))
+stopButton = Button(160,500,150,50,(0,0,255),"STOP",(0,0,0))
 
-    win = menuScreen.makeDisplay()
-    mapa = pg.image.load("mapCovid.png")
-    textBoxes = []
-    textBoxO = TextBox(218,29,100,50,4)
-    textBox1 = TextBox(700,700,100,50,4)
-    textBoxes.append(textBoxO)
-    textBoxes.append(textBox1)
-    running = True
+humansize = 1
+odmeraj = 0.1
+duzina = 500
+rangelow = 3
+rangehigh = duzina - rangelow
+
+mapa = pg.image.load('covid19\\NEWMAP.png')  
+
+black = (0, 0, 0, 255)
+white = (255, 255, 255, 255)
+healthycolor = (0, 255, 170)
+
+win = menuScreen.makeDisplay()
+    
+textBoxes = []
+textBoxO = TextBox(218,29,100,50,4)
+textBox1 = TextBox(700,700,100,50,4)
+textBoxes.append(textBoxO)
+textBoxes.append(textBox1)
+running = True
    
-    while running:
-        menuScreen.screenUpdate()
-        tempScreen.screenUpdate()
-        mousePos = pg.mouse.get_pos()
-        mouseClick = pg.mouse.get_pressed()
+while running:
+    menuScreen.screenUpdate()
+    tempScreen.screenUpdate()
+    mousePos = pg.mouse.get_pos()
+    mouseClick = pg.mouse.get_pressed()
+    if textBoxO.returnValue() == '':
+        n = 0
+    else:
+        n = int(textBoxO.returnValue())
 
-        tempDropButton = dropButton.isOver(mousePos, mouseClick)
-        if tempDropButton:
-            b = 170
-            i = 0
-            for i in range(5):
-                b += 30
-                textBox1 = TextBox(188,100,100,50,4)
-                textBoxes.append(textBox1)
-                textBox1.draw(menuScreen.returnTitle())
-                if i == 0:
-                    city = myfont.render(" novi sad",1,(255,255,0))
-                if i == 1:
-                    city = myfont.render(" new york",1,(255,255,0))
-                if i == 2:
-                    city = myfont.render("  milano",1,(255,255,0))
-                if i == 3:
-                    city = myfont.render("  valjevo",1,(255,255,0))
-                if i == 4:
-                    city = myfont.render("   svet",1,(255,255,0))
-                menuScreen.returnTitle().blit(city,(206,b-30))
-
-        if menuScreen.checkUpdate():
-            tempScreenButton = labelReady.isOver(mousePos,mouseClick)
-            if not tempDropButton:
-                labelReady.drawButton(menuScreen.returnTitle(),True)
-            menuScreen.returnTitle().blit(label,(0,40))
-            menuScreen.returnTitle().blit(labelCity, (0,130))
-            
-            textBoxO.draw(menuScreen.returnTitle())
+    tempDropButton = dropButton.isOver(mousePos, mouseClick)
+    if tempDropButton:
+        b = 170
+        i = 0
+        for i in range(5):
+            b += 30
+            textBox1 = TextBox(188,100,100,50,4)
+            textBoxes.append(textBox1)
             textBox1.draw(menuScreen.returnTitle())
-            dropButton.drawButton(menuScreen.returnTitle())
-            menuScreen.returnTitle().blit(labelSee, (200,150))
+            if i == 0:
+                city = myfont.render(" novi sad",1,(255,255,0))
+            if i == 1:
+                city = myfont.render(" new york",1,(255,255,0))
+            if i == 2:
+                city = myfont.render("  milano",1,(255,255,0))
+            if i == 3:
+                city = myfont.render("  valjevo",1,(255,255,0))
+            if i == 4:
+                city = myfont.render("   svet",1,(255,255,0))
+            menuScreen.returnTitle().blit(city,(206,b-30))
+
+    if menuScreen.checkUpdate():
+        simScreenButton = labelReady.isOver(mousePos,mouseClick)
+        if not tempDropButton:
+            labelReady.drawButton(menuScreen.returnTitle(),True)
+        menuScreen.returnTitle().blit(label,(0,40))
+        menuScreen.returnTitle().blit(labelCity, (0,130))
+            
+        textBoxO.draw(menuScreen.returnTitle())
+        textBox1.draw(menuScreen.returnTitle())
+        dropButton.drawButton(menuScreen.returnTitle())
+        menuScreen.returnTitle().blit(labelSee, (200,150))
    
-            if tempScreenButton:
-                simScreen.makeDisplay()
-                simulationPlay(textBoxO,simScreen)
-                menuScreen.endCurrent()
+        if simScreenButton:
+            win = simScreen.makeDisplay()
+                
+            menuScreen.endCurrent()
 
-        elif tempScreen.checkUpdate():
-            menuScreen.returnTitle().blit(mapa, (0,0))
-            returnButton = labelReturn.isOver(mousePos,mouseClick)
-            labelReturn.drawButton(tempScreen.returnTitle())
+    
+    elif simScreen.checkUpdate:
+        
+        #simScreen.returnTitle().fill((0, 0, 0))
+        simScreen.returnTitle().blit(mapa, (0, 0))
+        startButtonSim = startButton.isOver(mousePos,mouseClick)
+        stopButtonSim = stopButton.isOver(mousePos,mouseClick)
+        startButton.drawButton(simScreen.returnTitle())
+        stopButton.drawButton(simScreen.returnTitle())
 
-            if returnButton:
-                win = menuScreen.makeDisplay()
-                tempScreen.endCurrent()
-        elif simScreen.checkUpdate:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    win = menuScreen.makeDisplay()
-                    simScreen.endCurrent()
+        if startButtonSim:
+            threads = []
+            #start = time.perf_counter()
+            for _ in range(n):
+                
+                t = threading.Thread(target=spawn)   
+                t.start()
+                threads.append(t)
+                  
+
+            for thread in threads:
+                thread.join()
         
-        """if labelReady.collidepoint((mouseClick)):
-            if Click:
-                simulationPlay()"""
+
         
-        """pg.draw.rect(menuScreen.returnTitle(), (255,0,0), labelReady)"""
-        Click = False
-            #event
-        for event in pg.event.get():
             if event.type == pg.QUIT:
-                running = False
+                win = menuScreen.makeDisplay()
+                simScreen.endCurrent()
+        
+       
+    Click = False
+        #event
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
 
 
-            if event.type == pg.MOUSEBUTTONDOWN:
-                for box in textBoxes:
-                    box.checkClick(pg.mouse.get_pos())
-                if event.button:
-                    Click = True    
+        if event.type == pg.MOUSEBUTTONDOWN:
+            for box in textBoxes:
+                box.checkClick(pg.mouse.get_pos())
+            if event.button:
+                Click = True    
 
-            if event.type == pg.KEYDOWN:
-                for box in textBoxes:
-                    if box.active:
-                        box.addText(event.key)
+        if event.type == pg.KEYDOWN:
+            for box in textBoxes:
+                if box.active:
+                    box.addText(event.key)
                         
 
-        pg.display.update()
-        mainClock.tick(60)
-
-main()
+    pg.display.update()
+    mainClock.tick(60)
